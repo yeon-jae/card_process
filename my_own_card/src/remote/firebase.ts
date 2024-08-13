ec4e82a42a04d3036bc4282df45a8341a42e4a24
yeon-jae <lisanamyj99@gmail.com>
2024-08-13 10:41:42 +0900

    feat: Firebase설정

---
+++ my_own_card/src/remote/firebase.ts
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import {getAuth} from 'firebase/auth'
+import {getFirestore} from "firebase/firestore"
+
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENSER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MESASUREMENT_ID
+}=process.env
+
+
+
+const firebaseConfig = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain:REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket:REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENSER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MESASUREMENT_ID
+};
+
+// Initialize Firebase
+console.log(process.env)
+
+export const app = initializeApp(firebaseConfig)
+export const auth= getAuth(app)
+export const store= getFirestore(app);
