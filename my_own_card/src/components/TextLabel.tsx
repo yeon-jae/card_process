71ccffc4bd5a24a358c20a4a5e35d84ac6942826
yeon-jae <lisanamyj99@gmail.com>
2024-08-18 01:58:07 +0900

    feat: 공통 컴포넌트 _TextField기능 수정 중

--- my_own_card/src/components/TextLabel.tsx
+++
@@ -1,8 +0,0 @@
-import { InputHTMLAttributes } from "react";
-
-interface TextLabel extends InputHTMLAttributes<HTMLInputElement>{
-    label?:React.ReactNode
-    hasError?: boolean
-    helpMessage?:React.ReactNode
-
-}
---
+++ my_own_card/src/components/shared/TextField.tsx
@@ -0,0 +1,30 @@
+import { forwardRef, InputHTMLAttributes,useState } from "react";
+import Input from "./Input";
+import Text from "./Text";
+
+interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement>{
+    label?:React.ReactNode
+    hasError?: boolean
+    helpMessage?:React.ReactNode
+
+}
+const TextField= forwardRef<HTMLInputElement,TextFieldProps>(
+    function TextField({label,hasError, onFocus, onBlur,...props},ref){
+        const [focused,setFocused]=useState(false)
+        const labelColor=hasError?'red':focused?'blue':'grey'
+        const handleFocus=()=>{
+
+        }
+        const handleBlur=()=>{}
+
+        return(
+            <div>
+            {label?<Text typography="t7" color={labelColor} display="inline-block"
+            style={{marginBottom:6}}>{label}</Text>:null}
+       
+        <Input ref={ref} aria-invalid={hasError} onFocus={handleFocus} onBlur={handleBlur}{...props}/>
+        </div>
+        )
+        
+    }
+)
