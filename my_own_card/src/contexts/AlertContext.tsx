52243620f0c0e78cb12863a440e05ba556b4f209
yeon-jae <lisanamyj99@gmail.com>
2024-08-21 16:13:30 +0900

    feat: alert

---
+++ my_own_card/src/contexts/AlertContext.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps, createContext,useContext  } from "react";
+import { createPortal } from "react-dom";
+import Alert from "@shared/Alert";
+
+type AlertProps=ComponentProps<typeof Alert>
+type AlertOptions=Omit<AlertProps,'open'>
+
+interface AlertContextValue{
+    open:(options:AlertOptions)=>void
+}
+const Context=createContext<AlertContextValue|undefined>(undefined)
+const defaultValues:AlertProps={
+    open:false,
+    title: null,
+    description:null,
+    onButtonClick:()=>{},
+}
+function AlertContextProvider()
