c248ad8afdded859bc611867d841eb3b8ecf5372
yeon-jae <lisanamyj99@gmail.com>
2024-08-22 14:08:48 +0900

    chore: AlertContext수정

--- my_own_card/src/contexts/AlertContext.tsx
+++ my_own_card/src/contexts/AlertContext.tsx
@@ -1,18 +1,26 @@
-import { ComponentProps, createContext,useContext  } from "react";
+import { ComponentProps, createContext, useContext } from "react";
 import { createPortal } from "react-dom";
 import Alert from "@shared/Alert";
 
-type AlertProps=ComponentProps<typeof Alert>
-type AlertOptions=Omit<AlertProps,'open'>
+type AlertProps = ComponentProps<typeof Alert>
+type AlertOptions = Omit<AlertProps, 'open'>
 
-interface AlertContextValue{
-    open:(options:AlertOptions)=>void
+interface AlertContextValue {
+    open: (options: AlertOptions) => void
 }
-const Context=createContext<AlertContextValue|undefined>(undefined)
-const defaultValues:AlertProps={
-    open:false,
+const Context = createContext<AlertContextValue | undefined>(undefined)
+const defaultValues: AlertProps = {
+    open: false,
     title: null,
-    description:null,
-    onButtonClick:()=>{},
+    description: null,
+    onButtonClick: () => { },
 }
-function AlertContextProvider()
+function AlertContextProvider({ children }: { children: React.ReactNode }) {
+    const useState = [];
+    return (
+        <Context.Provider>
+            {children}
+            <Alert />
+        </Context.Provider>
+    )
+}
