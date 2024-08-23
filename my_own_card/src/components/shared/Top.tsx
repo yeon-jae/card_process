7c08605da3265e0d05e046ac7dc37f9a0c8d8f8c
yeon-jae <lisanamyj99@gmail.com>
2024-08-23 15:44:16 +0900

    feat: 카드 리스트 TOP부분 구현 - 카드 추천란 글씨 소개란 구현

---
+++ my_own_card/src/components/shared/Top.tsx
@@ -0,0 +1,22 @@
+import { css } from "@emotion/react";
+import Flex from "./Flex";
+import Text from "./Text";
+
+interface TopProps{
+    title:string
+    subTitle:string
+}
+
+function Top({title,subTitle}:TopProps){
+    return(
+        <Flex direction="column">
+            <Text bold={true} typography="t4">{title}</Text>
+            <Text typography="t7">{subTitle}</Text>
+        </Flex>
+    )
+}
+const containerStyles=css`
+    padding:24px;    
+`
+
+export default Top
--- my_own_card/src/pages/Home.tsx
+++ my_own_card/src/pages/Home.tsx
@@ -1,4 +1,12 @@
+import Top from "@shared/Top";
 function HomePage(){
-    return<div>홈</div>
+    return(
+        <div>
+            <Top 
+                title="혜택 좋은 카드"
+                subTitle="회원님을 위해서 좋은 카드를 모았어요" />
+
+        </div>
+    )
 }
 export default HomePage;
