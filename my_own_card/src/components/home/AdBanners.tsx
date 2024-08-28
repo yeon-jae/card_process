76f810141c179603dbd8160456c89c6fd5b11f24
yeon-jae <lisanamyj99@gmail.com>
2024-08-28 17:09:23 +0900

    chore:package,json 충돌 오류 해결

--- my_own_card/src/components/home/AdBanners.tsx
+++ my_own_card/src/components/home/AdBanners.tsx
@@ -3,21 +3,31 @@ import { useQuery } from "react-query";
 import { getAdBanners } from "@remote/adBanner";
 import Flex from "@shared/Flex";
 import Text from "@shared/Text";
-import { colors, Colors } from "@styles/colorPalette";
+import { colors } from "@styles/colorPalette";
+import styled from "@emotion/styled";
 
 
 function AdBanners() {
   return (
-    <Flex direction="column" css={bannerContainerStyles}>
-      <Text>배너 타이틀</Text>
-      <Text>배너 내용</Text>
-    </Flex>
+    <Container>
+      <Flex direction="column" css={bannerContainerStyles}>
+        <Text bold={true}>배너 타이틀</Text>
+        <Text typography="t7">배너 내용</Text>
+      </Flex>
+    </Container>
   )
 }
+
+
+const Container = styled.div`
+  papdding:24px;
+  `
 
 const bannerContainerStyles = css`
   padding:16px;
   background-color:${colors.grey};
   border-radius:4px;
 `
+
+
 export default AdBanners;
