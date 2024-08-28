d8337afa45ecfe4edd6e93763b06eee91425ea09
yeon-jae <lisanamyj99@gmail.com>
2024-08-28 09:45:12 +0900

    feat: useEffect추가

--- my_own_card/src/pages/Home.tsx
+++ my_own_card/src/pages/Home.tsx
@@ -1,8 +1,13 @@
 import Top from "@shared/Top";
-function HomePage(){
-    return(
+import { useEffect } from "react";
+import { getCards } from "@remote/card";
+function HomePage() {
+    useEffect(() => {
+        getCards()
+    }, [])
+    return (
         <div>
-            <Top 
+            <Top
                 title="혜택 좋은 카드"
                 subTitle="회원님을 위해서 좋은 카드를 모았어요" />
 
