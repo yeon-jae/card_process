09c0159b50433c560c5e9e2f897e7f536aa47bf0
yeon-jae <lisanamyj99@gmail.com>
2024-09-02 17:26:42 +0900

    feat: ListRow추가

--- my_own_card/src/components/home/CardList.tsx
+++ my_own_card/src/components/home/CardList.tsx
@@ -1,12 +1,24 @@
 import ListRow from "@shared/ListRow"
+import { useQuery } from "react-query"
+import { getCards } from "@/remote/card"
 
 function CardList() {
+  const { data } = useQuery(['cards'], () => getCards())
+  if (data == null) { return null }
   return (
     <div>
       <ul>
-        <ListRow
-          left={<div>left</div>}
-          contents={<ListRow.Texts title="" subTitle="" />} right={<div>right</div>} />
+        {data.map(() => {
+          return (
+            <ListRow
+              left={<div>left</div>}
+              contents={<ListRow.Texts title="타이틀" subTitle="서브타이틀" />}
+              right={<div>right</div>}
+              withArrow={true}
+            />
+          )
+        })}
+
       </ul>
     </div>
   )
--- my_own_card/src/components/shared/ListRow.tsx
+++ my_own_card/src/components/shared/ListRow.tsx
@@ -13,8 +13,9 @@ function ListRow({ left, contents, right, withArrow, onClick }: ListRowProps) {
   return (
     <Flex as="li" css={listRowContainerStyles} onClick={onClick}>
       <Flex>{left}</Flex>
-      <Flex>{right}</Flex>
       <Flex>{contents}</Flex>
+      <Flex>{right}</Flex>
+
       {withArrow ? <IconArrowRight /> : null}
 
     </Flex>
@@ -46,12 +47,14 @@ function ListRowTexts({
 
 function IconArrowRight() {
   return (
-    <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" width={20} height={20}>
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
       <title />
-      <path d="M322.7,128.4L423,233.4c6,5.8,9,13.7,9,22.4c0,8.7-3,16.5-9,22.4L322.7,383.6c-11.9,12.5-31.3,12.5-43.2,0  c-11.9-12.5-11.9-32.7,0-45.2l48.2-50.4h-217C93.7,288,80,273.7,80,256c0-17.7,13.7-32,30.6-32h217l-48.2-50.4  c-11.9-12.5-11.9-32.7,0-45.2C291.4,115.9,310.7,115.9,322.7,128.4z" />
+      <path d="M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z" />
     </svg>
   )
 }
+
+
 
 ListRow.Texts = ListRowTexts
 export default ListRow
